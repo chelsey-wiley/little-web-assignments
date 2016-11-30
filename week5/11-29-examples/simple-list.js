e57f4c4f438d6d75c1f35b9c589d4afdc41b19d7
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-11-30 12:45:29 -0600

    simple-list assignment

---
+++ week5/11-29-examples/simple-list.js
@@ -0,0 +1,15 @@
+var theList = document.querySelector('#the-list');
+console.log('it works')
+
+var groceryList = [
+  'sausage',
+  'shrimp',
+  'Texjoy',
+  'Dr Pepper',
+  'steak',
+  'Tostitos',
+  'cheese',
+  'gum'
+];
+
+//Add code here!
