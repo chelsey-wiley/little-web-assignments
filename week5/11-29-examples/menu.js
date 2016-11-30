37a852286a31783aafbc817574be7970345538dd
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-11-30 15:38:58 -0600

    gmail js completed

--- week5/11-29-examples/menu.js
+++ week5/11-29-examples/menu.js
@@ -3,14 +3,18 @@ var theMenu = document.querySelector('.regular-menu');
 //console.log('the menu', theMenu);
 
 theMenu.addEventListener('click', function(evt) {
-  console.log('clicked', evt.target, evt.target.tagName);
+  // console.log('clicked', evt.target);
+if (evt.target.tagName === "LI"){
 
-  if (evt.target.tagName === 'LI') {
-    evt.target.classList.toggle('clicked');}
+  //must have 'LI' or the padding below the li in the ul will show through and be clickable
+  var newMenu = document.querySelector('.clicked');
+
+  if(newMenu !== null) {
+    //!null means not equal to null. Equals there is something there
+    // console.log('turned off previously selected');
+    newMenu.classList.remove('clicked');
+  }
+
+  evt.target.classList.toggle('clicked');
+}
 });
-
-var newMenu = document.querySelector('.clicked');
-
-
-
-// //get a variable of all the things (via querySelector) that are clicked. Add a click event to toggle or remove the styling
