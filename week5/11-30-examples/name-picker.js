819e9ce5017ab5aa972d5a3be656f1a07c01840e
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-11-30 17:56:07 -0600

    octocats and name-picker from class

--- week5/11-30-examples/name-picker.js
+++ week5/11-30-examples/name-picker.js
@@ -1,36 +1,63 @@
 (function(){
 
+var names = ['Bucephalus', 'Comanche', 'Mareng', 'Copenhagen', 'Sergeant Reckless', 'The Byerley Turk', 'Pinto', 'Beautiful', 'Jim Key']
 var counter = 0;
-var decrement = document.querySelector('.decrement');//class must have .
+var decrement = document.querySelector('.decrement');
 var increment = document.querySelector('.increment');
 var display = document.querySelector('.display');
-var names = ['Bucephalus', 'Comanche', 'Mareng', 'Copenhagen', 'Sergeant Reckless', 'The Byerley Turk', 'Pinto', 'Beautiful', 'Jim Key']
 
-//add variable in display
-display.textContent = names[counter];
-//add paramenters event name and function
+function updateDisplay(){
+  display.textContent = 'Hello, ' + names[counter] + '!';
+}
+//^ Created a function to update the
+
+updateDisplay();
+//display.textContent = names[counter];
+
 decrement.addEventListener('click', function(){
   counter--;
   if (counter < 0){
     counter = names.length -1;
   }
 
-  display.textContent= names[counter];
-  console.log(counter);
+  updateDisplay();
+  //display.textContent = names[counter];
 });
 
-//add paramenters event name and function
+
 increment.addEventListener('click', function(){
   counter++;
 
   if (counter > names.length -1){
     counter = 0;
   }
-//counter goes up
-  display.textContent= names[counter];
+
+/*Or you can:
+if(position === name.length -1){
+positon = 0;
+}
+
+else {
+position ++;
+}
+*/
+
+  updateDisplay();
+  //display.textContent = names[counter];
+  
 //display the counter
 console.log(counter);
 });
 console.log(counter);
 
 })();
+
+/*
+1. add array of names to variable.
+2. variable = document.querySelector('.classname')
+---> add this for the buttons and the display
+3. set counter to 0 as a starting point
+--->better name for counter is position
+4. display.textContent = names[counter]
+5. addEventListener to the class you want to click on. using a click function
+*/
