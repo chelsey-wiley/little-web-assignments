efc55bf2f96b1e468fcb56fca761e59cbd9c25dd
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-11-30 22:13:13 -0600

    amazon basic css transitions no JS added yet simple-list exmple js added to comments

--- week5/11-29-examples/simple-list.js
+++ week5/11-29-examples/simple-list.js
@@ -12,4 +12,29 @@ var groceryList = [
   'gum'
 ];
 
-//Add code here!
+theList.addEventListener('click' function(evt){
+
+});
+
+
+
+//
+// var theMenu = document.querySelector('.regular-menu');
+// //console.log('the menu', theMenu);
+//
+// theMenu.addEventListener('click', function(evt) {
+//   // console.log('clicked', evt.target);
+// if (evt.target.tagName === "LI"){
+//
+//   //must have 'LI' or the padding below the li in the ul will show through and be clickable
+//   var newMenu = document.querySelector('.clicked');
+//
+//   if(newMenu !== null) {
+//     //!null means not equal to null. Equals there is something there
+//     // console.log('turned off previously selected');
+//     newMenu.classList.remove('clicked');
+//   }
+//
+//   evt.target.classList.toggle('clicked');
+// }
+// });
