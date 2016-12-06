ce297f88a6a8dd386603fd4dcd69db564d4baada
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-05 18:43:33 -0600

    minor changes to clean up code and js make your own adventure example

--- week5/12-1-examples/toggle.js
+++ week5/12-1-examples/toggle.js
@@ -1,10 +1,21 @@
 
-var clickHere = document.querySelector('.click-here');
+var clickHere = document.querySelectorAll('.click-here');
 
 clickHere.addEventListener('click', function(){
 
     var text = document.querySelector('.text');
 
     text.classList.toggle('toggle-it')
+
+});
+
+
+var barbie = document.querySelector('.barbie');
+
+barbie.addEventListener('click', function(){
+
+    var ken = document.querySelector('.barbie');
+
+    ken.classList.toggle('ken')
 
 });
