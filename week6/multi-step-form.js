17201a48caedc9fd18e49565606e47cf7439b279
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-06 16:09:18 -0600

    multi-step

--- week6/multi-step-form.js
+++ week6/multi-step-form.js
@@ -1,15 +1,16 @@
 console.log('hello world')
 
+
 var nextWhere = document.querySelector('.next-where');
 
 nextWhere.addEventListener('click', function(){
   var show = document.querySelector('.where');
-  show.classList.add('on')
+  show.classList.remove('on')
 });
 
 var nextWhat = document.querySelector('.next-what');
 
 nextWhat.addEventListener('click', function(){
   var show = document.querySelector('.what');
-  show.classList.add('on')
+  show.classList.remove('on')
 });
