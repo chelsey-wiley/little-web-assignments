8b36463dd13fda1322d01a8fb6e9ec1f24a65b6a
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-06 16:38:56 -0600

    header shrinks AND expands

--- week6/scroll-event.js
+++ week6/scroll-event.js
@@ -4,6 +4,11 @@ window.addEventListener('scroll', function(evt) {
     var tallHeader =document.querySelector('.tall-header')
     tallHeader.classList.add('fix-it');
     console.log('nope!');
-  }
+  };
 
+  if (window.pageYOffset < 201) {
+   var fixIt =document.querySelector('.fix-it')
+   fixIt.classList.remove('fix-it');
+   console.log('yes!');
+ };
 });
