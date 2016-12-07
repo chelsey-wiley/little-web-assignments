59db9ccb7312df1046ea7fcc0b79f0b6b3b6d50b
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-06 21:37:14 -0600

    created new more readable functions calling functions with example support

--- week6/multi-step-form.js
+++ week6/multi-step-form.js
@@ -1,16 +1,62 @@
 console.log('hello world')
 
+var next1 = document.querySelector('.who .next');
+var next2 = document.querySelector('.where .next');
+var back2 = document.querySelector('.where .back');
+var back3 = document.querySelector('.what .back');
 
-var nextWhere = document.querySelector('.next-where');
-
-nextWhere.addEventListener('click', function(){
-  var show = document.querySelector('.where');
-  show.classList.remove('on')
+next1.addEventListener('click', function(){
+  hideTheDiv();
+  show('.where');
 });
 
-var nextWhat = document.querySelector('.next-what');
-
-nextWhat.addEventListener('click', function(){
-  var show = document.querySelector('.what');
-  show.classList.remove('on')
+next2.addEventListener('click', function(){
+  hideTheDiv();
+  show('.what');
 });
+
+back2.addEventListener('click', function(){
+  hideTheDiv();
+  show('.who');
+});
+
+back3.addEventListener('click', function(){
+  hideTheDiv();
+  show('.where');
+});
+
+function hideTheDiv(){
+  var showing = document.querySelector('.showing');
+  showing.classList.remove('showing');
+}
+
+function show(selector){
+  var thing = document.querySelector(selector);
+  thing.classList.add('showing');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// var nextWhere = document.querySelector('.next-where');
+//
+// nextWhere.addEventListener('click', function(){
+//   var show = document.querySelector('.where');
+//   show.classList.remove('on')
+// });
+//
+// var nextWhat = document.querySelector('.next-what');
+//
+// nextWhat.addEventListener('click', function(){
+//   var show = document.querySelector('.what');
+//   show.classList.remove('on')
+// });
