fd2078dbac9c71c7b94791dd1043db213a566646
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-08 09:41:30 -0600

    again text change

--- week6/12-8-examples/script.js
+++ week6/12-8-examples/script.js
@@ -3,18 +3,19 @@ console.log('script file hooked up!');
 var clickItButton = document.querySelector('.click-it');
 var clickThisButton = document.querySelector('.click-this');
 var cold =document.querySelector ('.cold');
-// var click1 = document.querySelector('.click-1');
-// var click2 = document.querySelector('.click-2');
 
 
 clickItButton.addEventListener ('click', function(){
-  cold.textContent = "I wish it was warm!";
-  cold.classList.toggle('on');
+  theMessage("Burrr it's cold");
+
 });
 
 clickThisButton.addEventListener ('click', function(){
-  cold.textContent = "I wish it was warm!";
-  cold.classList.toggle('on');
+  theMessage("I wish it was warm");
+
 });
 
-v
+function theMessage(placeholder){
+  cold.textContent = placeholder;
+  cold.classList.toggle('on');
+}
