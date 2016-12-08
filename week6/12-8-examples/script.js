bed06008b54200a8d13da5322539566d27290cf0
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-08 09:32:23 -0600

    again-with notes

--- week6/12-8-examples/script.js
+++ week6/12-8-examples/script.js
@@ -4,16 +4,21 @@ var clickItButton = document.querySelector('.click-it');
 var clickThisButton = document.querySelector('.click-this');
 var cold = document.querySelector ('.cold');
 
-
 clickItButton.addEventListener ('click', function(){
-  changeIt()
+  namedChangeIt(); // -- FUNCTION CALLED HERE
+  // var cold =document.querySelector ('.cold'); -- MOVE VARIABLE
+  // cold.textContent = "I wish it was warm!"; -- CREATE FUNCTION
+  // cold.classList.toggle('on'); -- CREATED FUNCTION
 });
 
 clickThisButton.addEventListener ('click', function(){
-  changeIt()
+  namedChangeIt(); // -- FUNCTION CALLED HERE
+  // var cold =document.querySelector ('.cold'); -- MOVE VARIABLE
+  // cold.textContent = "I wish it was warm!"; -- CREATE FUNCTION
+  // cold.classList.toggle('on'); -- CREATE FUNCTION
 });
 
-function changeIt(){
+function namedChangeIt(){
   cold.textContent = "I wish it was warm!";
   cold.classList.toggle('on');
-}
+};
