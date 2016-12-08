00daf518fc1e8dd90118f3c5d3e7a027ec14f892
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-08 09:25:53 -0600

    again

--- week6/12-8-examples/script.js
+++ week6/12-8-examples/script.js
@@ -2,15 +2,15 @@ console.log('script file hooked up!');
 
 var clickItButton = document.querySelector('.click-it');
 var clickThisButton = document.querySelector('.click-this');
+var cold = document.querySelector ('.cold');
+
 
 clickItButton.addEventListener ('click', function(){
-  var cold =document.querySelector ('.cold');
   cold.textContent = "I wish it was warm!";
   cold.classList.toggle('on');
 });
 
 clickThisButton.addEventListener ('click', function(){
-  var cold =document.querySelector ('.cold');
   cold.textContent = "I wish it was warm!";
   cold.classList.toggle('on');
 });
