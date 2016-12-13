56aa10b7a6f400b7ce8a9a0247286511ba12bba6
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-12 20:35:58 -0600

    committing completed readtime

--- week6/readtime/script.js
+++ week6/readtime/script.js
@@ -1,1 +1,16 @@
 console.log('script file hooked up!');
+
+var para =document.querySelectorAll("p");
+var readTime = document.querySelector('.readtime');
+
+
+var sum = 0;
+
+for (var i = 0; i < para.length; i++) {
+  var theArray = para[i].textContent.split(" ");
+  sum = sum + theArray.length;
+}
+
+var result = Math.floor(sum/300)
+readTime.textContent =result;
+console.log(result);
