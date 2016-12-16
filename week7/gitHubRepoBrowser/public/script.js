800865a032c6d6f335814c59ce9adbb20654bfea
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2016-12-16 15:39:30 -0600

    starting weekend assignment

--- week7/gitHubRepoBrowser/public/script.js
+++ week7/gitHubRepoBrowser/public/script.js
@@ -1,16 +1,39 @@
 console.log('script file hooked up!');
 
-var gifList=document.querySelector('.gif-list');
+var resList=document.querySelector('#res-list');
 
-$.ajax({
-  url: 'http://api.giphy.com/v1/gifs/search?q=megatron&api_key=dc6zaTOxFJmzC&offset=0'
-})
 
-.done(function(apiResult){
-  console.log(data);
+function requestData(){
+  var promise = $.ajax({
+    url: 'https://api.github.com/search/repositories?q=barbie'
+  });
 
-  for (var i = 0; i < apiResult.data.length; i++) {
-    var obj = apiResult.data[i];
-    console.log(obj);
-  }
-})
+  promise.done(function(data){
+    console.log(data);
+
+    // for (var i = 0; i < data.length; i++) {
+    //   var search = [i];
+    //   console.log(apiResult);
+    // }
+  })
+};
+requestData();
+
+var wordInput = document.querySelector('.search-text');
+wordInput.addEventListener('keyup',function(evt){
+  // console.log(evt.keyCode);
+  if(evt.keyCode===13){
+    console.log('you hit enter');
+
+  $.ajax({
+  url:'https://api.github.com/search/repositories?q=',
+  method:'POST',
+  data: { word: wordInput.value }
+  })
+
+  .done(function(){
+    console.log('the POST is done')
+    getTheData();
+  });
+}
+});
