5427cd62bb9948b4ae9525d7e1f0432cc6b06fdc
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2017-01-03 14:28:04 -0600

    paging is working

--- week7/gitHubRepoBrowser/public/script.js
+++ week7/gitHubRepoBrowser/public/script.js
@@ -6,30 +6,34 @@ var nextButton = document.querySelector('.next');
 var backButton = document.querySelector('.back');
 var currentPage = document.querySelector('.current-page');
 var page = 1;
+var pageResults = 10;
+// var totalResults = data.total_count;
+//if `totalResults` is hoisted to the top "data is not defined" if totalResults is defined within the function then data never gets passed through. can't get total results until an api is called.
 
-nextButton.addEventListener('click', function(){
-  page +=1;
-  localStorage.setItem('the-page-is', page);
-  requestData();
-  currentPage.innerHTML = page;
-  console.log ('next clicked')
-});
-
-
-backButton.addEventListener('click', function(){
-  page -=1;
-  localStorage.setItem('the-page-is', page);
-  requestData();
-  currentPage.innerHTML = page;
-  console.log('back clicked')
-});
-
-
+// nextButton.addEventListener('click', function(){
+//   if (page < data.total_count / 20){
+//     page +=1;
+//     localStorage.setItem('the-page-is', page);
+//     requestData();
+//     currentPage.innerHTML = page;
+//     console.log ('next clicked')
+//   }
+// });
+//
+//
+// backButton.addEventListener('click', function(){
+//   page -=1;
+//   localStorage.setItem('the-page-is', page);
+//   requestData();
+//   currentPage.innerHTML = page;
+//   console.log('back clicked')
+// });
 
 
 function requestData(){
+  var wordHere = wordInput.value + "&page=" + page;
   var promise = $.ajax({
-    url: 'https://api.github.com/search/repositories?q=' + wordInput.value
+    url: 'https://api.github.com/search/repositories?q=' + wordHere
   });
 
   promise.done(function(data){
@@ -37,6 +41,7 @@ function requestData(){
       var searchInfo = document.querySelector('.search-info');
       var templateScript = document.querySelector('.results-template');
       var templateHtml=templateScript.innerHTML;
+
 
       var html =''
 
@@ -49,9 +54,29 @@ function requestData(){
       }
 
       searchInfo.innerHTML= html;
+      totalCount = data.total_count;
 
   })
 };
+
+nextButton.addEventListener('click', function(){
+  if (page < totalCount / 30){
+    page +=1;
+    localStorage.setItem('the-page-is', page);
+    requestData();
+    currentPage.innerHTML = page;
+    console.log ('next clicked')
+  }
+});
+
+
+backButton.addEventListener('click', function(){
+  page -=1;
+  localStorage.setItem('the-page-is', page);
+  requestData();
+  currentPage.innerHTML = page;
+  console.log('back clicked')
+});
 
 wordInput.addEventListener('keyup',function(evt){
   // console.log(evt.keyCode);
