5e2334974d6905939cd53b6a4ef29a9f01b0c657
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2017-01-06 16:20:41 -0600

    everything but links

--- week9/ReactGithubBrowser/client/js/script.js
+++ week9/ReactGithubBrowser/client/js/script.js
@@ -1,8 +1,59 @@
 var mountNode = document.querySelector('#react-root');
 
 class AppComponent extends React.Component {
+
+  componentDidMount(){
+    this.theInput.focus();
+  }
+
+  constructor(){
+  super();
+  this.state ={
+    apiResult:{
+      items:[]
+    }
+  }
+  console.log('getting the info');
+}
+
+  clicky(){
+    console.log('the input', this.theInput, this.theInput.value);
+    this.theData(this.theInput.value);
+  }
+
+  keyUp(evt) {
+   console.log(evt.keyCode, evt.target);
+   if (evt.keyCode === 13) {
+     this.theData(evt.target.value);
+   }
+ }
+
+  theData(){
+    $.ajax({
+      url:"https://api.github.com/search/users?q=" + this.theInput.value
+    })
+    .done((data)=> {
+      console.log('got the data', data);
+      this.setState({
+        apiResult:data
+      });
+    });
+  }
+
   render() {
-    return <div>React works!</div>;
+    return <div className="data-component">
+      <input placeholder ="search" onKeyUp={(evt) => {this.keyUp(evt); }} ref={(theDomElement) => {this.theInput = theDomElement;}}/>
+      <button onClick={() => {this.clicky(); }}>enter</button>
+
+    <ul className="results">
+      {this.state.apiResult.items.map((info) => {return <li className="theList" key={info.id}>
+        <div className="name">{info.login}</div>
+        <div className="url">{info.url}</div>
+      </li>; })}
+    </ul>
+
+
+    </div>
   }
 }
 
--- week9/ReactGithubBrowser/public/script.js
+++ week9/ReactGithubBrowser/public/script.js
@@ -13,19 +13,95 @@ var mountNode = document.querySelector('#react-root');
 var AppComponent = function (_React$Component) {
   _inherits(AppComponent, _React$Component);
 
+  _createClass(AppComponent, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this.theInput.focus();
+    }
+  }]);
+
   function AppComponent() {
     _classCallCheck(this, AppComponent);
 
-    return _possibleConstructorReturn(this, (AppComponent.__proto__ || Object.getPrototypeOf(AppComponent)).apply(this, arguments));
+    var _this = _possibleConstructorReturn(this, (AppComponent.__proto__ || Object.getPrototypeOf(AppComponent)).call(this));
+
+    _this.state = {
+      apiResult: {
+        items: []
+      }
+    };
+    console.log('getting the info');
+    return _this;
   }
 
   _createClass(AppComponent, [{
+    key: 'clicky',
+    value: function clicky() {
+      console.log('the input', this.theInput, this.theInput.value);
+      this.theData(this.theInput.value);
+    }
+  }, {
+    key: 'keyUp',
+    value: function keyUp(evt) {
+      console.log(evt.keyCode, evt.target);
+      if (evt.keyCode === 13) {
+        this.theData(evt.target.value);
+      }
+    }
+  }, {
+    key: 'theData',
+    value: function theData() {
+      var _this2 = this;
+
+      $.ajax({
+        url: "https://api.github.com/search/users?q=" + this.theInput.value
+      }).done(function (data) {
+        console.log('got the data', data);
+        _this2.setState({
+          apiResult: data
+        });
+      });
+    }
+  }, {
     key: 'render',
     value: function render() {
+      var _this3 = this;
+
       return React.createElement(
         'div',
-        null,
-        'React works!'
+        { className: 'data-component' },
+        React.createElement('input', { placeholder: 'search', onKeyUp: function onKeyUp(evt) {
+            _this3.keyUp(evt);
+          }, ref: function ref(theDomElement) {
+            _this3.theInput = theDomElement;
+          } }),
+        React.createElement(
+          'button',
+          { onClick: function onClick() {
+              _this3.clicky();
+            } },
+          'enter'
+        ),
+        React.createElement(
+          'ul',
+          { className: 'results' },
+          this.state.apiResult.items.map(function (info) {
+            return React.createElement(
+              'li',
+              { className: 'theList', key: info.id },
+              React.createElement(
+                'div',
+                { className: 'name' },
+                info.login
+              ),
+              React.createElement(
+                'div',
+                { className: 'url' },
+                info.url
+              )
+            );
+          })
+        )
       );
     }
   }]);
